cd862fc8ee1dc44f4f1b15e2a91977a3671abcd0
Alexander Kolovatov <a.kolovatov@gmail.com>
2019-06-04 12:29:26 +0600

    2.3.CompoundTypes

--- C++_chapter2_excercises/main.cpp
+++ C++_chapter2_excercises/main.cpp
@@ -44,5 +44,65 @@ int main(int argc, const char * argv[]) {
     
     cout << "'i' equals " << i << " and 'sum' equals " << sum << endl;
     
+#pragma mark 2.3 - Compound Types
+#pragma mark 2.3.1 References
+    
+    // 'rvalue reference'
+    // when we use the term reference, we mean 'Ivalue reference'
+    
+    int ival = 1024;
+    int &refVal = ival; // refVal refers to (is another name for) ival
+    
+    // int &refVal2; // error: a reference must be initialized
+    // bind - связывать. Because there is no way to rebind a reference, references must be initialized.
+    // A Reference Is an Alias - ссылка это псевдоним
+    
+    refVal = 2;
+    
+    // All objects have same value
+    cout << ival << " 'ival'" << endl; // same memory reference
+    cout << refVal << " 'refVal'" << endl; // same memory reference
+    
+    cout << &refVal << " refVal memory address" << endl; // 0x7ffeefbff49c
+    cout << &ival << " ival memory address"  << endl; // 0x7ffeefbff49c
+    
+    int ii = refVal; // define new reference to refVal
+    int &newVal = ii;
+    
+    cout << ii << " 'ii'" << endl;
+    cout << &newVal << " newVal memory address" << endl; // 0x7ffeefbff48c
+    cout << &ii << " ii memory address" << endl; // 0x7ffeefbff48c
+    
+    newVal = 56;
+    
+    cout << newVal << " = 'newVal' " << ii << " = 'ii'" << endl;
+    cout << refVal << " = 'refVal " << ival << " = 'ival'" << endl;
+    
+#pragma mark Ex. 2.15
+    
+    /*(a)*/ int exival = 1.01; // cut fraction part, means exival = 1
+    /*(b)   int &rval1 = 1.01; error */
+    /*(c)*/ int &rval2 = ival;
+    cout << rval2 << " rval2" << endl;
+    cout << &rval2 << " memory address 'rval2'" << endl; // 0x7ffeefbff49c same as 'ival'
+    /*(d)   int &rval3; requires an initializer */
+    
+#pragma mark Ex. 2.16
+    int newI = 0;
+    // &r1 = i; invalid
+    double d = 0; // add fractional part .00
+    // &r2 = d; // use undeclaried identifier
+    
+    /*(a) r2 = 3.14159; error */
+    /*(b) r2 = r1; error */
+    /*(c) newI = r2; error */
+    /*(d) r1 = d; error */
+    
+#pragma mark Ex. 2.17
+    int k;
+    // &rk = k; error
+    k = 5;
+    // rk = 10; error
+    
     return 0;
 }
